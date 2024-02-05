9903795b72a15341f5c6aebc683530b4863c03bc
wangjun <wb.wangjun21@mesg.corp.netease.com>
2024-02-05 16:46:29 +0800

    fix:解决removeWatermark问题

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import loadMark from './loadMark'
 import { monitorDom,getWH } from './util'
 import { DEFAULT_SETTINGS } from './constants'
+/* 删除水印标记 */
+ let delMap=new Map()
 /* 存水印配置，以id 为键 */
 export const customSettingMap = new Map()
 /*加载水印-添加浏览器窗口监听事件-添加dom变化监听事件*/
@@ -10,7 +12,7 @@ export function watermark(settings = {}) {
     loadMark(settings)
     // 添加浏览器窗口监听事件
     window.addEventListener('resize', function () {
-        loadMark(settings)
+        !delMap.get(settings.id)&&loadMark(settings)
     })
     const watermarkId = settings.id || DEFAULT_SETTINGS.id
     const watermarkParentSelector = settings.parentSelector || DEFAULT_SETTINGS.parentSelector
@@ -18,13 +20,13 @@ export function watermark(settings = {}) {
     const isSupportMutationObserve = monitorDom(watermarkParentSelector, (mutations, observer) => {
         // 当节点被删除
         if (mutations[0].removedNodes[0] && mutations[0].removedNodes[0].id === watermarkId) {
-            loadMark(settings)
+           !delMap.get(settings.id)&&loadMark(settings)
         }
     })
     // 不支持MutationObserve时的降级方案Mutation Events
     if (!isSupportMutationObserve) {
         observeNode.addEventListener('DOMNodeRemoved', function () {
-            loadMark(settings)
+            !delMap.get(settings.id)&&loadMark(settings)
         }, false)
     }
 }
@@ -34,10 +36,11 @@ export function removeWatermark(watermarkId) {
     if (watermarkDom) {
         watermarkDom.parentNode.removeChild(watermarkDom)
     }
+    delMap.set(watermarkId, true)
 }
 /* 获取水印配置 */
 export function getWatermarkConfig(watermarkId) {
-    return customSettingMap.get(watermarkId)
+    return customSettingMap.get(watermarkId)||{}
  }
 /* 计算水印宽高 */
 export const getSingleWH = getWH
